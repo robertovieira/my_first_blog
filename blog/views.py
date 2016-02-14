621a750f44aa9a94c4cca861313a6c079797f8be
Roberto Vieira <robertto.vierra@gmail.com>
2016-02-14 01:06:01 -0300

    Controle de acesso adicionado e funcionando.

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import redirect
-from django.shortcuts import render, get_object_or_404
-from django.utils import timezone
-from .models import Post
-from .forms import PostForm
+from django.contrib.auth.decorators import login_required #controle de acesso
+from django.shortcuts import redirect # redirecionamento
+from django.shortcuts import render, get_object_or_404 # para retornar uma pagina amigavel
+from django.utils import timezone # para usar a funcao timezone.now() e etc
+from .models import Post # modelo usado por enquanto
+from .forms import PostForm # classe do formulario de New Post
 
 # Create your views here.
 
@@ -14,6 +15,7 @@ def post_detail(request, pk):
 	post = get_object_or_404(Post, pk = pk)
 	return render(request, 'blog/post_detail.html', {'post': post})	
 
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -27,6 +29,7 @@ def post_new(request):
 
     return render(request, 'blog/post_edit.html', {'form': form})
 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -40,12 +43,19 @@ def post_edit(request, pk):
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
 
+@login_required
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
 
+@login_required
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.publish()
-    #return render(request, 'blog/post_detail.html', {'pk': post.pk})
     return redirect('blog.views.post_detail', pk=post.pk)
+
+@login_required
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('blog.views.post_list')
