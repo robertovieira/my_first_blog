d6b3fff34bb8911ee3e9ea669402641b4f4cc110
Roberto Vieira <robertto.vierra@gmail.com>
2016-02-15 23:49:41 -0300

    Mudança no layout

---
+++ blog/migrations/0003_auto_20160215_2224.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('blog', '0002_comment'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='comment',
+            options={'permissions': (('blog_comment_approve', 'Aprovar Comentário'),)},
+        ),
+    ]
--- blog/models.py
+++ blog/models.py
@@ -20,18 +20,25 @@ class Post(models.Model):
 		return self.title
 
 	def approved_comments(self):
-		return self.comments.filter(approved_comment=True)
+		return self.comments.filter(approved_comment=True)	
 
 class Comment(models.Model):
-    post = models.ForeignKey('blog.Post', related_name='comments')
-    author = models.CharField(max_length=200)
-    text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now)
-    approved_comment = models.BooleanField(default=False)
+	post = models.ForeignKey('blog.Post', related_name='comments')
+	author = models.CharField(max_length=200)
+	text = models.TextField()
+	created_date = models.DateTimeField(default=timezone.now)
+	approved_comment = models.BooleanField(default=False)
 
-    def approve(self):
-        self.approved_comment = True
-        self.save()
+	def approve(self):
+		self.approved_comment = True
+		self.save()
 
-    def __str__(self):
-        return self.text    
+	def __str__(self):
+		return self.text
+
+	class Meta:
+		permissions = (
+			("blog_comment_approve", "Aprovar Comentário"),
+		)
+
+		ordering = ['-created_date']
